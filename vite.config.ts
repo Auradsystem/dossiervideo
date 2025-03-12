9fee4c5bf9baa89772343edbbbf8ad00166b54e2
Auradsystem <sammari.dali@gmail.com>
2025-03-12 19:51:10 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,13 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-    exclude: ['lucide-react'],
-  },
-  resolve: {
-    alias: {
-      '@mui/icons-material': '@mui/icons-material/esm',
-      '@mui/material': '@mui/material/esm',
-    },
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+  },
+  server: {
+    hmr: false
   },
 })
